6657b47c9db4b3e68e5c4685a515e71a07862871
tharunkumarsk <tharunkumarsk@gmail.com>
2019-11-22 15:53:51 +0100

    refactor: Extract game variable in beforeEach

--- test/ExampleTest.js
+++ test/ExampleTest.js
@@ -4,22 +4,23 @@ const assert = require("chai").assert
 
 describe('Game', function () {
 
+    var game;
+
+    this.beforeEach(function(){
+        game = new Game();
+    });
+
     it('X- should be the currentPlayer', function () {
-        var game = new Game();
         assert.equal(game.currentPlayer ,"X")
     });
 
     it('should record X move on the board on correct position', function(){
-        var game = new Game();
-        
         game.play("X", 8);
 
         assert.equal(game.gameBoard[8], 'X');
     });
 
     it('Should "O" be the next player after X', function(){
-        var game = new Game();
-
         game.play("X", 8);
 
         assert.equal(game.currentPlayer,"O")
